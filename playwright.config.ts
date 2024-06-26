2b97a7d26c100e84fae9eed7ab20a5c90e15ca8e
MarinaBidenko <bidenko.bmw@gmai.com>
2024-06-26 14:54:13 +0300

    Added html reporter on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,14 +21,16 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   timeout: 100 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
+  reporter: [
+    ['html', {open: 'on-failure'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
