8bbe516a28262c3bad0543a714e56ebc3d41867d
MarinaBidenko <bidenko.bmw@gmai.com>
2024-06-30 16:50:13 +0300

    Added script, nvmrc, added mistake in test for pipline test

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   timeout: 100 * 1000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', {open: 'on-failure'}]
+    ['html', {open: 'on-failure'}],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
