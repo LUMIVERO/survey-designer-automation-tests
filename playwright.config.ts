a804027394254c2a9755996ff63e5a508b2a2fb1
MarinaBidenko <bidenko.bmw@gmai.com>
2024-06-30 18:18:49 +0300

    Added allure-reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', {open: 'never'}],
+    ["allure-playwright"]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
